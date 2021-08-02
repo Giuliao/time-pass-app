e5e7760535af55f61f74933e1e887c785d8bd862
Giuliao <616488283@qq.com>
2021-08-02 22:58:33 +0800

    21.8.2修复渲染阻塞计时延迟问题

--- src/app/pages/stop-watch/stop-watch.component.ts
+++ src/app/pages/stop-watch/stop-watch.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, OnDestroy} from '@angular/core';
 
 const MINUTES = 60;
-
+const SECONDS = 60;
+const MICRO_SECONDS = 1000;
+const UNIT = SECONDS * MICRO_SECONDS;
 @Component({
   selector: 'app-stop-watch',
   templateUrl: './stop-watch.component.html',
@@ -12,35 +14,50 @@ export class StopWatchComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit(): void {
+    // 请求允许通知
+    Notification.requestPermission(function(status){});
   }
 
   ngOnDestroy(): void {
     this.stop();
   }
-  public time = 45 * MINUTES;
+
   public intervalId: any;
   public timeValue: number = 45;
-  public calcTime : number = 45 * MINUTES;
+  public calcTime : number = 45;
+  private calcGap : number = 0;
+  private startTime: number = 0;
 
   public get percentageText() : string {
-    if(this.time > 60) {
-      return `${(this.time / MINUTES).toFixed(0)} min`;
+    if(this.calcGap > this.timeValueByMiroSeconds ) {
+      return '0';
     }
-    return `${(this.time).toFixed(0)} s`;
+    const restTime = this.timeValueByMiroSeconds - this.calcGap;
+    if(restTime > UNIT) {
+      return `${(restTime / UNIT).toFixed(0)} min`;
+    }
+
+    return `${(restTime / MICRO_SECONDS).toFixed(0)} s`;
   }
 
   public get percentage(): number {
-    return this.time / this.calcTime * 100;
+    if(this.calcGap > this.timeValueByMiroSeconds) {
+      return 0;
+    }
+
+    const restTime = this.timeValueByMiroSeconds - this.calcGap;
+
+    return (restTime / this.timeValueByMiroSeconds)* 100;
   }
 
 
   public start() {
     this.stop();
-    this.time = this.timeValue * MINUTES;
-    this.calcTime = this.timeValue * MINUTES;
+    this.startTime = Date.now();
+    this.calcTime = this.timeValue;
     this.intervalId = setInterval(()=>{
-      this.time -= 1;
-      if(this.time <= 0) {
+      this.calcGap = Date.now() - this.startTime 
+      if(this.calcGap >= this.timeValueByMiroSeconds) {
         new Notification('时间到')
         this.stop();
       }
@@ -52,6 +69,10 @@ export class StopWatchComponent implements OnInit, OnDestroy {
       clearInterval(this.intervalId);
       this.intervalId = null;
     }
+  }
+
+  private get timeValueByMiroSeconds(): number {
+    return this.calcTime * UNIT;
   }
 
 }
